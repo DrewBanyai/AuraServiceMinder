6733fd9d01b4167da81e00fd23554607e7011994
Drew Banyai <DrewBanyai@gmail.com>
2026-03-16 03:40:29 -0400

    Moved icon, image, and font declarations to their own declarations.h header

--- aura/aura.ino
+++ aura/aura.ino
@@ -9,6 +9,7 @@
 #include <Preferences.h>
 #include "esp_system.h"
 #include "translations.h"
+#include "declarations.h"
 
 #define XPT2046_IRQ 36   // T_IRQ
 #define XPT2046_MOSI 32  // T_DIN
@@ -30,34 +31,7 @@
 #define NIGHT_MODE_START_HOUR 22
 #define NIGHT_MODE_END_HOUR 6
 
-LV_FONT_DECLARE(lv_font_montserrat_latin_12);
-LV_FONT_DECLARE(lv_font_montserrat_latin_14);
-LV_FONT_DECLARE(lv_font_montserrat_latin_16);
-LV_FONT_DECLARE(lv_font_montserrat_latin_20);
-LV_FONT_DECLARE(lv_font_montserrat_latin_42);
-
 static Language current_language = LANG_EN;
-
-// Font selection based on language
-const lv_font_t* get_font_12() {
-  return &lv_font_montserrat_latin_12;
-}
-
-const lv_font_t* get_font_14() {
-  return &lv_font_montserrat_latin_14;
-}
-
-const lv_font_t* get_font_16() {
-  return &lv_font_montserrat_latin_16;
-}
-
-const lv_font_t* get_font_20() {
-  return &lv_font_montserrat_latin_20;
-}
-
-const lv_font_t* get_font_42() {
-  return &lv_font_montserrat_latin_42;
-}
 
 SPIClass touchscreenSPI = SPIClass(VSPI);
 XPT2046_Touchscreen touchscreen(XPT2046_CS, XPT2046_IRQ);
@@ -109,62 +83,6 @@ static lv_obj_t *clock_24hr_switch;
 static lv_obj_t *night_mode_switch;
 static lv_obj_t *language_dropdown;
 static lv_obj_t *lbl_clock;
-
-// Weather icons
-LV_IMG_DECLARE(icon_blizzard);
-LV_IMG_DECLARE(icon_blowing_snow);
-LV_IMG_DECLARE(icon_clear_night);
-LV_IMG_DECLARE(icon_cloudy);
-LV_IMG_DECLARE(icon_drizzle);
-LV_IMG_DECLARE(icon_flurries);
-LV_IMG_DECLARE(icon_haze_fog_dust_smoke);
-LV_IMG_DECLARE(icon_heavy_rain);
-LV_IMG_DECLARE(icon_heavy_snow);
-LV_IMG_DECLARE(icon_isolated_scattered_tstorms_day);
-LV_IMG_DECLARE(icon_isolated_scattered_tstorms_night);
-LV_IMG_DECLARE(icon_mostly_clear_night);
-LV_IMG_DECLARE(icon_mostly_cloudy_day);
-LV_IMG_DECLARE(icon_mostly_cloudy_night);
-LV_IMG_DECLARE(icon_mostly_sunny);
-LV_IMG_DECLARE(icon_partly_cloudy);
-LV_IMG_DECLARE(icon_partly_cloudy_night);
-LV_IMG_DECLARE(icon_scattered_showers_day);
-LV_IMG_DECLARE(icon_scattered_showers_night);
-LV_IMG_DECLARE(icon_showers_rain);
-LV_IMG_DECLARE(icon_sleet_hail);
-LV_IMG_DECLARE(icon_snow_showers_snow);
-LV_IMG_DECLARE(icon_strong_tstorms);
-LV_IMG_DECLARE(icon_sunny);
-LV_IMG_DECLARE(icon_tornado);
-LV_IMG_DECLARE(icon_wintry_mix_rain_snow);
-
-// Weather Images
-LV_IMG_DECLARE(image_blizzard);
-LV_IMG_DECLARE(image_blowing_snow);
-LV_IMG_DECLARE(image_clear_night);
-LV_IMG_DECLARE(image_cloudy);
-LV_IMG_DECLARE(image_drizzle);
-LV_IMG_DECLARE(image_flurries);
-LV_IMG_DECLARE(image_haze_fog_dust_smoke);
-LV_IMG_DECLARE(image_heavy_rain);
-LV_IMG_DECLARE(image_heavy_snow);
-LV_IMG_DECLARE(image_isolated_scattered_tstorms_day);
-LV_IMG_DECLARE(image_isolated_scattered_tstorms_night);
-LV_IMG_DECLARE(image_mostly_clear_night);
-LV_IMG_DECLARE(image_mostly_cloudy_day);
-LV_IMG_DECLARE(image_mostly_cloudy_night);
-LV_IMG_DECLARE(image_mostly_sunny);
-LV_IMG_DECLARE(image_partly_cloudy);
-LV_IMG_DECLARE(image_partly_cloudy_night);
-LV_IMG_DECLARE(image_scattered_showers_day);
-LV_IMG_DECLARE(image_scattered_showers_night);
-LV_IMG_DECLARE(image_showers_rain);
-LV_IMG_DECLARE(image_sleet_hail);
-LV_IMG_DECLARE(image_snow_showers_snow);
-LV_IMG_DECLARE(image_strong_tstorms);
-LV_IMG_DECLARE(image_sunny);
-LV_IMG_DECLARE(image_tornado);
-LV_IMG_DECLARE(image_wintry_mix_rain_snow);
 
 void create_ui();
 void fetch_and_update_weather();
---
+++ aura/declarations.h
@@ -0,0 +1,87 @@
+#ifndef DECLARATIONS_H
+#define DECLARATIONS_H
+
+LV_FONT_DECLARE(lv_font_montserrat_latin_12);
+LV_FONT_DECLARE(lv_font_montserrat_latin_14);
+LV_FONT_DECLARE(lv_font_montserrat_latin_16);
+LV_FONT_DECLARE(lv_font_montserrat_latin_20);
+LV_FONT_DECLARE(lv_font_montserrat_latin_42);
+
+// Font selection based on language
+const lv_font_t* get_font_12() {
+  return &lv_font_montserrat_latin_12;
+}
+
+const lv_font_t* get_font_14() {
+  return &lv_font_montserrat_latin_14;
+}
+
+const lv_font_t* get_font_16() {
+  return &lv_font_montserrat_latin_16;
+}
+
+const lv_font_t* get_font_20() {
+  return &lv_font_montserrat_latin_20;
+}
+
+const lv_font_t* get_font_42() {
+  return &lv_font_montserrat_latin_42;
+}
+
+// Weather icons
+LV_IMG_DECLARE(icon_blizzard);
+LV_IMG_DECLARE(icon_blowing_snow);
+LV_IMG_DECLARE(icon_clear_night);
+LV_IMG_DECLARE(icon_cloudy);
+LV_IMG_DECLARE(icon_drizzle);
+LV_IMG_DECLARE(icon_flurries);
+LV_IMG_DECLARE(icon_haze_fog_dust_smoke);
+LV_IMG_DECLARE(icon_heavy_rain);
+LV_IMG_DECLARE(icon_heavy_snow);
+LV_IMG_DECLARE(icon_isolated_scattered_tstorms_day);
+LV_IMG_DECLARE(icon_isolated_scattered_tstorms_night);
+LV_IMG_DECLARE(icon_mostly_clear_night);
+LV_IMG_DECLARE(icon_mostly_cloudy_day);
+LV_IMG_DECLARE(icon_mostly_cloudy_night);
+LV_IMG_DECLARE(icon_mostly_sunny);
+LV_IMG_DECLARE(icon_partly_cloudy);
+LV_IMG_DECLARE(icon_partly_cloudy_night);
+LV_IMG_DECLARE(icon_scattered_showers_day);
+LV_IMG_DECLARE(icon_scattered_showers_night);
+LV_IMG_DECLARE(icon_showers_rain);
+LV_IMG_DECLARE(icon_sleet_hail);
+LV_IMG_DECLARE(icon_snow_showers_snow);
+LV_IMG_DECLARE(icon_strong_tstorms);
+LV_IMG_DECLARE(icon_sunny);
+LV_IMG_DECLARE(icon_tornado);
+LV_IMG_DECLARE(icon_wintry_mix_rain_snow);
+
+// Weather Images
+LV_IMG_DECLARE(image_blizzard);
+LV_IMG_DECLARE(image_blowing_snow);
+LV_IMG_DECLARE(image_clear_night);
+LV_IMG_DECLARE(image_cloudy);
+LV_IMG_DECLARE(image_drizzle);
+LV_IMG_DECLARE(image_flurries);
+LV_IMG_DECLARE(image_haze_fog_dust_smoke);
+LV_IMG_DECLARE(image_heavy_rain);
+LV_IMG_DECLARE(image_heavy_snow);
+LV_IMG_DECLARE(image_isolated_scattered_tstorms_day);
+LV_IMG_DECLARE(image_isolated_scattered_tstorms_night);
+LV_IMG_DECLARE(image_mostly_clear_night);
+LV_IMG_DECLARE(image_mostly_cloudy_day);
+LV_IMG_DECLARE(image_mostly_cloudy_night);
+LV_IMG_DECLARE(image_mostly_sunny);
+LV_IMG_DECLARE(image_partly_cloudy);
+LV_IMG_DECLARE(image_partly_cloudy_night);
+LV_IMG_DECLARE(image_scattered_showers_day);
+LV_IMG_DECLARE(image_scattered_showers_night);
+LV_IMG_DECLARE(image_showers_rain);
+LV_IMG_DECLARE(image_sleet_hail);
+LV_IMG_DECLARE(image_snow_showers_snow);
+LV_IMG_DECLARE(image_strong_tstorms);
+LV_IMG_DECLARE(image_sunny);
+LV_IMG_DECLARE(image_tornado);
+LV_IMG_DECLARE(image_wintry_mix_rain_snow);
+
+#endif //  DECLARATIONS_H
